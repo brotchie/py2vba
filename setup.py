4b7dded92ab103cf9593157456ea30e378419ab7
James Brotchie <brotchie@gmail.com>
2012-01-12 12:06:43 +1000

    Added setuptools setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='py2vba',
+      version='1.0',
+      description='Python to VBA Translator',
+      author='James Brotchie',
+      author_email='brotchie@gmail.com',
+      url='https://bitbucket.org/brotchie/py2vba',
+      packages=['py2vba'],
+      requires=['excelbt'],
+     )
